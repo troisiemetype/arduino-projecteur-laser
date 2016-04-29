992b5cb0c8c13b0add31df2bf9a6513eb0c14ae4
Pierre-Loup Martin <pierreloupm@gmail.com>
2016-04-29 18:48:42 +0200

    Added licence GNU GPL3.

--- I2C.cpp
+++ I2C.cpp
@@ -1,5 +1,22 @@
 // Arduino laser projector
-
+/*
+ * This program is intended to control a laser projector
+ * Copyright (C) 2016  Pierre-Loup Martin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
 // I2C.cpp
 // This part of the program sends and receives datas trough I2C.
 
--- settings.h
+++ settings.h
@@ -1,4 +1,21 @@
 // Arduino laser projector
+/*
+ * This program is intended to control a laser projector
+ * Copyright (C) 2016  Pierre-Loup Martin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 // settings.h
 // This part of the program stores the settings used by the other parts.
